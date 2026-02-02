a947a28386b6333edbc2570251cb62b0bda7c9a4
Nguyntanh <nguyntanh2k5@gmail.com>
2026-02-02 20:49:08 +0700

    cấu hình lại tailwindcss và tạo giao diện landing page

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,ts}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#2B59FF',
+        surface: '#F8FAFC',
+      },
+    },
+  },
+  plugins: [],
+};
