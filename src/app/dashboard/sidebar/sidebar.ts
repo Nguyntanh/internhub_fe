beb0d531fe1a6b5b230edc3f62f88d87ec258c87
Nguyntanh <nguyntanh2k5@gmail.com>
2026-02-25 11:24:27 +0700

    sửa sự kiện ẩn hiện sidebar để vẫn hiện icon menu tabs

--- src/app/dashboard/sidebar/sidebar.ts
+++ src/app/dashboard/sidebar/sidebar.ts
@@ -1,12 +1,13 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Import CommonModule
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [CommonModule], // Add CommonModule to imports
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
   encapsulation: ViewEncapsulation.None,
 })
 export class Sidebar {
-
+  @Input() isSidebarVisible: boolean = true; // Input to receive sidebar visibility state
 }
