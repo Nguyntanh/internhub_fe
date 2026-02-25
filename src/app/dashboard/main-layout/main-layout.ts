a2429586d7ca2e801d4c2dc4709b12b0556e31a4
Nguyntanh <nguyntanh2k5@gmail.com>
2026-02-25 11:03:25 +0700

    roll back về giao diện cũ khi làm ẩn hiện sidebar

--- src/app/dashboard/main-layout/main-layout.ts
+++ src/app/dashboard/main-layout/main-layout.ts
@@ -11,5 +11,9 @@ import { RouterModule } from '@angular/router'; // Import RouterModule
   encapsulation: ViewEncapsulation.None,
 })
 export class MainLayout {
+  isSidebarOpen: boolean = true; // State to control sidebar visibility
 
+  toggleSidebar() {
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
 }
--- src/app/dashboard/topbar/topbar.ts
+++ src/app/dashboard/topbar/topbar.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, EventEmitter, Input, Output, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-topbar',
@@ -8,5 +8,10 @@ import { Component, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None,
 })
 export class Topbar {
+  @Input() isSidebarOpen: boolean = true; // Input to receive sidebar state
+  @Output() toggleSidebar = new EventEmitter<void>(); // Output to emit toggle event
 
+  onToggleSidebar() {
+    this.toggleSidebar.emit();
+  }
 }
