d6f357df866a491406acbe9353799acbc0ca9145
pquangminh05 <phamminhheo2k5@gmail.com>
2026-03-04 16:36:50 +0700

    quan ly phong ban

--- src/app/settings/hr/hr.ts
+++ src/app/settings/hr/hr.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-hr',
-  imports: [],
-  templateUrl: './hr.html',
-  styleUrl: './hr.css',
+  standalone: true,
+  imports: [RouterModule],
+  template: `<router-outlet></router-outlet>`
 })
-export class Hr {
-
-}
+export class Hr {}
