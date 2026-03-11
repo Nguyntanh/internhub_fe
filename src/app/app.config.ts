130a719421e5a87067003295be56760519dbba4c
Nguyen Tien Sy <troioidungaigiongtoi4@gmail.com>
2026-03-11 17:29:40 +0700

    giao diện quản lý nhật lý hệ thống

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,18 +1,21 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, HTTP_INTERCEPTORS, withInterceptorsFromDi } from '@angular/common/http'; // Import provideHttpClient and HTTP_INTERCEPTORS
-
+import {
+  provideHttpClient,
+  HTTP_INTERCEPTORS,
+  withInterceptorsFromDi,
+  withFetch,
+} from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { JwtInterceptor } from './auth/jwt.interceptor'; // Import JwtInterceptor
+import { JwtInterceptor } from './auth/jwt.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
-    provideHttpClient(withInterceptorsFromDi()), // Add provideHttpClient here
-    // Cấu hình JwtInterceptor để tự động đính kèm token vào mọi request
+    provideHttpClient(withFetch(), withInterceptorsFromDi()),
     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
   ],
 };
